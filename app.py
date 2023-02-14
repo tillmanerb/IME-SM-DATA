c3d3ef40a9c1289e525502cd02692f692e23193e
rpi-sm02 <tierb@calpoly.edu>
2023-02-13 17:06:26 -0800

    manual update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from dash import Dash, dcc, html, Input, Output
 import os
+import pandas as pd
+import plotly.express as px
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -8,19 +10,20 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+machinedata = pd.read_csv('/home/rpi-sm02/out/out.csv')
+
+#drawing visual #1
+fig = px.line(machinedata, x = "Timestamp", y=  "Coolant")
+
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
+    html.H2('Development Dashboard'),
+    dcc.graph(
+        figure = fig,
+        id = 'devgraph
     ),
     html.Div(id='display-value')
 ])
 
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
